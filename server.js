7d3364d703c950ea5b2f7241d27e8a66e298d4bc
Jonathan Cordero <cryptcur00101@protonmail.com>
2020-03-05 20:39:09 -0500

    done

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const db = require("./db");
+
+const PORT = process.send.PORT || 3000;
+
+app.get("/", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "./index.html"));
+});
+app.get("/api/products", (req, res, next) => {
+  db.readJSON("./products.json")
+    .then(products => res.send(products))
+    .catch(ex => next(ex));
+});
+
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
