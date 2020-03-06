9215a1e567e5b946ae5b086f635ad46743dc33e1
Denis McPhillips <denismcphillips@gmail.com>
2020-03-05 21:24:46 -0500

    end of class

--- server.js
+++ server.js
@@ -8,9 +8,18 @@ const PORT = process.send.PORT || 3000;
 app.get("/", (req, res, next) => {
   res.sendFile(path.join(__dirname, "./index.html"));
 });
+
 app.get("/api/products", (req, res, next) => {
   db.readJSON("./products.json")
     .then(products => res.send(products))
+    .catch(ex => next(ex));
+});
+
+app.post("/api/products", (req, res, next) => {});
+
+app.get("/api/companies", (req, res, next) => {
+  db.readJSON("./companies.json")
+    .then(companies => res.send(companies))
     .catch(ex => next(ex));
 });
 
